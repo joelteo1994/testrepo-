3e72299a03b2f7b56615c591326e15881907575b
joelteo1994 <45536384+joelteo1994@users.noreply.github.com>
2024-09-11 12:14:50 +0800

    Update spacex_dash_app.py

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -65,7 +65,7 @@ def get_pie_chart(entered_site):
 
 # TASK 4:
 # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
-@app callback(Output(component_id='success-payload-scatter-chart', component_property='figure'), 
+@app.callback(Output(component_id='success-payload-scatter-chart', component_property='figure'), 
             [Input(component_id='site-dropdown', component_property='value'), Input(component_id='payload-slider', component_property='value')])
 
 def get_scatter_chart(entered_site, payload_range):
